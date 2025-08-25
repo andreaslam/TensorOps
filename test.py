d196e444f458f39b9cf4fded709750a3d858b512
andreaslam <andreaslamkk.official@gmail.com>
2025-08-25 22:38:28 +0100

    deleted:    tensorops/a.py 	deleted:    test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from tinygrad import Tensor
-
-a = Tensor.randn(1024, 512)
-b = Tensor.randn(512, 1024)
-aa = a.reshape([1024, 1, 512]).expand([1024, 1024, 512])
-bb = b.permute([1, 0]).reshape([1, 1024, 512]).expand([1024, 1024, 512])
-cc = (aa * bb).sum(axis=2)  # matmul result
-print("a@b", (a @ b).numpy())
-print("cc", (cc).numpy())
